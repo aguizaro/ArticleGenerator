3502b3820a96c975f712094d70c8a26e5e88ed93
Antonio Guizar Orozco <aguizaro@ucsc.edu>
2024-04-13 22:44:43 -0700

    add project files

---
+++ main.js
@@ -0,0 +1,48 @@
+// find elements
+const app = document.getElementById("app");
+const dropdown = document.getElementById("dropdown-button");
+const dropdownMenu = document.getElementsByClassName("dropdown-content")[0];
+const generateButton = document.getElementById("generate-button");
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+const articleTitle = document.getElementById("article-title");
+const articleImg = document.getElementById("article-img");
+const articleContent = document.getElementById("article-content");
+const articleElement = document.getElementsByClassName("article")[0];
+const articleEndpoint = `http://54.201.254.45:3000/article?key=${
+  import.meta.env.VITE_ACCESS_KEY
+}&category=`;
+
+const getCurrentCategory = () => {
+  const checkedRadio = Array.from(radioButtons).find((radio) => radio.checked);
+  return checkedRadio.value;
+};
+
+const generateArticle = async () => {
+  try {
+    generateButton.disabled = true;
+    articleElement.classList.remove("is-active");
+
+    const category = getCurrentCategory();
+    const response = await fetch(`${articleEndpoint}${category}`);
+    const data = await response.json();
+
+    console.log(data.response);
+
+    articleElement.classList.add("is-active");
+    articleTitle.textContent = data.response.title;
+    articleImg.src = data.response.urlToImage;
+    articleContent.textContent = data.response.content;
+
+    generateButton.disabled = false;
+    dropdownMenu.classList.remove("is-active");
+  } catch (error) {
+    console.error(error);
+    generateButton.disabled = false;
+  }
+};
+
+dropdown.addEventListener("click", () => {
+  dropdownMenu.classList.toggle("is-active");
+});
+
+generateButton.addEventListener("click", generateArticle);
