6ca99d21f4c72eb5595fcb7513895b8b273ff1b3
Antonio Guizar Orozco <aguizaro@ucsc.edu>
2025-02-05 21:32:51 -0800

    updated link

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is a web interface for a generator that produces satirical news articles similar to those found on [The Onion](https://www.theonion.com/). I've developed a [backend service](https://github.com/aguizaro/NewsArticleGenerator) capable of handling requests for articles based on specified categories.
 
-Check out the [demo](https://aguizaro.github.io/ArticleGenerator/).
+Check out the [demo](https://letsgeneratearticles.com/).
--- main.js
+++ main.js
@@ -13,115 +13,120 @@ const articleElement = document.querySelector(".article");
 const downloadDiv = document.querySelector(".download");
 const weblink = document.getElementById("weblink");
 const downloadButton = document.getElementById("download-button");
-const articleEndpoint = `https://letsgeneratearticles.com/article?key=AIzaSyD5&category=`;
-const generatedEndpoint = `https://letsgeneratearticles.com/generated?key=AIzaSyD5&seed=`;
+const articleEndpoint = `http://127.0.0.1:3000/article?key=AIzaSyD5&category=`;
+const generatedEndpoint = `http://127.0.0.1:3000/generated?key=AIzaSyD5&seed=`;
 
 // functions -------------------------------------------------------------
 
 // get the current category selected
 const getCurrentCategory = () => {
-  const checkedRadio = Array.from(radioButtons).find((radio) => radio.checked);
-  return checkedRadio.value;
+	const checkedRadio = Array.from(radioButtons).find((radio) => radio.checked);
+	return checkedRadio.value;
 };
 
 // capture the screen based on DOM elements and dwonload as image
 const captureScreen = async () => {
-  const articleScreen = await html2canvas(articleElement, {
-    logging: true,
-    ignoreElements: (element) => element.id === "download-button",
-    backgroundColor:
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? "#242424"
-        : "#ffffff",
-  });
+	const articleScreen = await html2canvas(articleElement, {
+		logging: true,
+		ignoreElements: (element) => element.id === "download-button",
+		backgroundColor:
+			window.matchMedia &&
+				window.matchMedia("(prefers-color-scheme: dark)").matches
+				? "#242424"
+				: "#ffffff",
+	});
 
-  const link = document.createElement("a");
-  link.href = articleScreen.toDataURL();
-  link.download = "article-screenshot.png";
-  link.click();
+	const link = document.createElement("a");
+	link.href = articleScreen.toDataURL();
+	link.download = "article-screenshot.png";
+	link.click();
 };
 
 // handle download button click
 const downloadArticle = async () => {
-  try {
-    downloadButton.disabled = true;
-    await captureScreen();
-    downloadButton.disabled = false;
-  } catch (error) {
-    console.error(error);
-    downloadButton.disabled = false;
-  }
+	try {
+		downloadButton.disabled = true;
+		await captureScreen();
+		downloadButton.disabled = false;
+	} catch (error) {
+		console.error(error);
+		downloadButton.disabled = false;
+	}
 };
 
 // fetch article from API given the category
 const generateArticle = async () => {
-  try {
-    generateButton.disabled = true;
-    articleElement.classList.remove("is-active");
+	try {
+		generateButton.disabled = true;
+		articleElement.classList.remove("is-active");
 
-    const category = getCurrentCategory();
-    const response = await fetch(`${articleEndpoint}${category}`);
-    const data = await response.json();
+		const category = getCurrentCategory();
 
-    console.log(data.response);
+		console.log(`${articleEndpoint}${category}`);
 
-    populateArticle(data);
+		const response = await fetch(`${articleEndpoint}${category}`);
+		const data = await response.json();
 
-    generateButton.disabled = false;
-    dropdownMenu.classList.remove("is-active");
-  } catch (error) {
-    console.error(error);
-    generateButton.disabled = false;
-  }
+		console.log(data.response);
+
+		populateArticle(data);
+
+		generateButton.disabled = false;
+		dropdownMenu.classList.remove("is-active");
+	} catch (error) {
+		console.error(error);
+		generateButton.disabled = false;
+	}
 };
 
 // check for seed in URL and fetch previous article - does nothing if no seed
 const checkForSeed = async () => {
-  const urlParams = new URLSearchParams(window.location.search);
-  const seed = urlParams.get("seed");
+	const urlParams = new URLSearchParams(window.location.search);
+	const seed = urlParams.get("seed");
 
-  if (seed) {
-    try {
-      generateButton.disabled = true;
-      articleElement.classList.remove("is-active");
+	if (seed) {
+		try {
+			generateButton.disabled = true;
+			articleElement.classList.remove("is-active");
 
-      const response = await fetch(`${generatedEndpoint}${seed}`);
-      if (response.status !== 200) {
-        throw new Error("Seed not found");
-      }
-      const data = await response.json();
+			console.log(`${generatedEndpoint}${seed}`);
 
-      console.log(data.response);
+			const response = await fetch(`${generatedEndpoint}${seed}`);
+			if (response.status !== 200) {
+				throw new Error("Seed not found");
+			}
+			const data = await response.json();
 
-      populateArticle(data);
+			console.log(data.response);
 
-      generateButton.disabled = false;
-      dropdownMenu.classList.remove("is-active");
-    } catch (error) {
-      console.error(error);
-      generateButton.disabled = false;
-    }
-  }
+			populateArticle(data);
+
+			generateButton.disabled = false;
+			dropdownMenu.classList.remove("is-active");
+		} catch (error) {
+			console.error(error);
+			generateButton.disabled = false;
+		}
+	}
 };
 
 // populate the article with given json data
 const populateArticle = (data) => {
-  articleElement.classList.add("is-active");
-  articleTitle.textContent = data.response.title;
-  articleImg.src = `data:image/jpeg;base64,${data.response.urlToImage}`;
-  articleContent.textContent = data.response.content;
-  const generatedLink =
-    window.location.origin +
-    window.location.pathname +
-    `?seed=${data.response.seed}`;
-  weblink.textContent = generatedLink;
-  window.history.replaceState({}, "", generatedLink);
+	articleElement.classList.add("is-active");
+	articleTitle.textContent = data.response.title;
+	articleImg.src = `data:image/jpeg;base64,${data.response.urlToImage}`;
+	articleContent.textContent = data.response.content;
+	const generatedLink =
+		window.location.origin +
+		window.location.pathname +
+		`?seed=${data.response.seed}`;
+	weblink.textContent = generatedLink;
+	window.history.replaceState({}, "", generatedLink);
 };
 
 // event listeners -------------------------------------------------------------
 dropdown.addEventListener("click", () => {
-  dropdownMenu.classList.toggle("is-active");
+	dropdownMenu.classList.toggle("is-active");
 });
 
 generateButton.addEventListener("click", generateArticle);
